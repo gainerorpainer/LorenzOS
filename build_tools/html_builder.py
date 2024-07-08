ece85b347c57d4aee7564dd2a7b34fe68494887b
PainerOrGainer <istmirechtegal@outlook.com>
2024-07-08 08:13:33 +0200

    Simplified html builder

--- build_tools/html_builder.py
+++ build_tools/html_builder.py
@@ -2,47 +2,52 @@
 from glob import glob
 from pathlib import Path
 from os.path import exists
+from io import TextIOWrapper
+
+BINDATA_BLOCKSIZE = 32
 
 
-def _build_html(html_path: str, include_folder: str):
-    with open(html_path, "r", encoding="utf8") as f:
-        filecontent = f.readlines()
-
-    friendly_name = Path(html_path).stem
-
-    with open(f"{include_folder}\\static_html_{friendly_name}.h", "w+", encoding="utf8") as f:
-        f.write("#pragma once\n\n\n#include <Arduino.h>\n\n")
-        f.write(
-            f"static const char STATIC_HTML_{friendly_name.upper()}[] ICACHE_RODATA_ATTR =\n")
-        for line in filecontent:
-            # strip trailing \n, escape quotes
-            line = line[:-1].replace('"', '\\"')
-            # insert a c-style newline
-            f.write('"' + line + '\\r\\n"\n')
-        f.write(";\n")
+def _build_preamble(file: TextIOWrapper):
+    file.write("#pragma once\n\n\n#include <Arduino.h>\n\n")
 
 
-def _build_favicon(html_folder: str, include_folder: str):
+def _build_favicon(html_folder: str, file: TextIOWrapper):
     with open(f"{html_folder}\\favicon.ico", "rb") as f:
         binary_content = f.read()
 
-    with open(f"{include_folder}\\static_html_favicon.h", "w+", encoding="utf8") as f:
-        f.write("#pragma once\n\n#include <Arduino.h>\n\n")
-        f.write(
-            "static const byte STATIC_HTML_FAVICON_CONTENT[] ICACHE_RODATA_ATTR = { \\\n")
-        blocksize = 32
-        for block in [binary_content[i:i + blocksize] for i in range(0, len(binary_content), blocksize)]:
-            f.write("\t" + ", ".join(map(lambda b: "0x%02x" % b, block)) + ", \\\n")
-        f.write("};\n")
+    file.write(
+        "static const byte STATIC_HTML_FAVICON_CONTENT[] ICACHE_RODATA_ATTR = { \\\n")
+    for block in [binary_content[i:i + BINDATA_BLOCKSIZE] for i in range(0, len(binary_content), BINDATA_BLOCKSIZE)]:
+        file.write(
+            "\t" + ", ".join(map(lambda b: "0x%02x" % b, block)) + ", \\\n")
+    file.write("};\n")
+
+
+def _build_html(html_filepath: str, file: TextIOWrapper):
+    with open(html_filepath, "r", encoding="utf8") as f:
+        filecontent = f.readlines()
+    name = Path(html_filepath).stem.upper()
+    file.write(f"static const char STATIC_HTML_{name}[] ICACHE_RODATA_ATTR =\n")
+    for line in filecontent:
+        # strip trailing \n, escape quotes
+        line = line[:-1].replace('"', '\\"')
+        # insert a c-style newline
+        file.write('"' + line + '\\r\\n"\n')
+    file.write(";\n")
 
 
 def build_html(html_folder: str, include_folder: str):
     """Builds all encountered html files
     """
-    if exists(f"{html_folder}\\favicon.ico"):
-        _build_favicon(html_folder, include_folder)
-        print("Built \"favicon.ico\"")
+    with open(f"{include_folder}\\codegen_html.h", "w+", encoding="utf8") as f:
+        _build_preamble(f)
 
-    for html_filepath in glob(f"{html_folder}\\*.html"):
-        _build_html(html_filepath, include_folder)
-        print(f"Built \"{html_filepath}\"")
+        if exists(f"{html_folder}\\favicon.ico"):
+            _build_favicon(html_folder, f)
+            f.write("\n")
+            print("Built \"favicon.ico\"")
+
+        for html_filepath in glob(f"{html_folder}\\*.html"):
+            _build_html(html_filepath, f)
+            f.write("\n")
+            print(f"Built \"{html_filepath}\"")
